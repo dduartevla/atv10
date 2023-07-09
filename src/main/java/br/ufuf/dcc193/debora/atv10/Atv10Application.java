886a60c233d46c994fa7d0551f8aa30db7ebde5b
dduartevla <deboraduarte@ice.ufjf.br>
2023-07-09 16:55:59 -0300

    Commit Inicial

---
+++ src/main/java/br/ufuf/dcc193/debora/atv10/Atv10Application.java
@@ -0,0 +1,13 @@
+package br.ufuf.dcc193.debora.atv10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atv10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atv10Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufuf/dcc193/debora/atv10/Atv10ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufuf.dcc193.debora.atv10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atv10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
